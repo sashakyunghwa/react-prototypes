437ec4a3b4db9bc604ef6420362fad4a1d7e96c8
Queen Bae <sashakyunghwa@gmail.com>
2018-03-16 18:55:07 -0700

    react proto intro

--- src/index.js
+++ src/index.js
@@ -1,7 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+function luckyNumber(){
+    return Math.floor((Math.random() * 1000) + 1);
+};
+
+function greeting(user){
+   return <div className="container">
+       <h1>Hello person</h1>
+       <h2 className="text-muted">This is your lucky number: {user.luckyNumber}.</h2>
+       </div>
+};
+
+const user = {
+    name: 'Sasha',
+    luckyNumber: luckyNumber()
+}
+
 ReactDOM.render(
-    <h1 className="container">React Prototypes!</h1>,
+    greeting(user),
     document.getElementById('root')
 );
+
+
