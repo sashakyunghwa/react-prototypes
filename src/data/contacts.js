9fc967ba3557733be9fc0295babef4be2892d1c1
Queen Bae <sashakyunghwa@gmail.com>
2018-03-28 00:02:46 -0700

    finished forms-inputs prototype

---
+++ src/data/contacts.js
@@ -0,0 +1,20 @@
+export default [
+    {
+        firstName: 'Sasha',
+        lastName: 'Ross',
+        phone: '562-451-6783',
+        email: 'wassup@gmail.com'
+    },
+    {
+        firstName: 'Clark',
+        lastName: 'Kent',
+        phone: '562-673-8973',
+        email: 'whosyourdaddy@gmail.com'
+    },
+    {
+        firstName: 'Princess',
+        lastName: 'Barbie',
+        phone: '714-123-4567',
+        email: 'blondie@gmail.com'
+    }
+];
